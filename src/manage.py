0d826f7478bee2b3d8d052495a09d4adf4256c06
B.T. Franklin <brandon.franklin@gmail.com>
2026-01-17 00:28:32 -0700

    Improve error message formatting in manage.py and update password validator name formatting in settings.py

--- src/manage.py
+++ src/manage.py
@@ -9,7 +9,8 @@ def main() -> None:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH?"
+            "Couldn't import Django. Are you sure it's installed and available on your "
+            "PYTHONPATH?"
         ) from exc
     execute_from_command_line(sys.argv)
 
